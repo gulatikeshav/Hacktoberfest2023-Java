723efc47c44389289bd0c4bf6d3f725bf2e068c1
gulatikeshav <133638164+gulatikeshav@users.noreply.github.com>
2023-10-10 12:50:04 +0530

    Add a program Remove_Duplicates.java

---
+++ EasyDifficulty/src/Remove_Duplicates.java
@@ -0,0 +1,12 @@
+class Remove_Duplicates {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+
+        for (const int num : nums)
+         if (i < 1 || num > nums[i - 1])
+        nums[i++] = num;
+
+    return i;
+    }
+};
